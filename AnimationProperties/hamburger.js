fd29eac197203e3222baa034940d2a4724b355c1
audreyhou <audrey.artncreation@gmail.com>
2020-09-07 16:48:33 -0700

    Fixed menu animation

--- AnimationProperties/hamburger.js
+++ AnimationProperties/hamburger.js
@@ -3,19 +3,8 @@ var $ = jQuery;
 $(document).ready(function () {
     $(".HamburgerIcon").click(function () {
         $(this).toggleClass("is-active");
-        if(document.getElementById("MenuItem-Menu").style.display == "block")
-        {
-            document.getElementById("MenuItem-Menu").style.display = "none";
-            document.getElementById("MenuItem-About").style.display = "block";
-            document.getElementById("MenuItem-Experience").style.display = "block";
-            document.getElementById("MenuItem-Photography").style.display = "block";
-        }
-        else
-        {
-            document.getElementById("MenuItem-Menu").style.display = "block";
-            document.getElementById("MenuItem-About").style.display = "none";
-            document.getElementById("MenuItem-Experience").style.display = "none";
-            document.getElementById("MenuItem-Photography").style.display = "none";
-        }
+        $("#MenuItem-About").toggle(500);
+        $("#MenuItem-Experience").toggle(500);
+        $("#MenuItem-Photography").toggle(500);
     });
 });
