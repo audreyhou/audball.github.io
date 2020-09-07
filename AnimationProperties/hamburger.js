1483d6e2c549dd9e1253b9b6c6106466478e0b6e
audreyhou <audrey.artncreation@gmail.com>
2020-09-07 16:17:28 -0700

    added elements to menu, toggle when on click

--- AnimationProperties/hamburger.js
+++ AnimationProperties/hamburger.js
@@ -3,5 +3,19 @@ var $ = jQuery;
 $(document).ready(function () {
     $(".HamburgerIcon").click(function () {
         $(this).toggleClass("is-active");
+        if(document.getElementById("MenuItem-Menu").style.display == "block")
+        {
+            document.getElementById("MenuItem-Menu").style.display = "none";
+            document.getElementById("MenuItem-About").style.display = "block";
+            document.getElementById("MenuItem-Experience").style.display = "block";
+            document.getElementById("MenuItem-Photography").style.display = "block";
+        }
+        else
+        {
+            document.getElementById("MenuItem-Menu").style.display = "block";
+            document.getElementById("MenuItem-About").style.display = "none";
+            document.getElementById("MenuItem-Experience").style.display = "none";
+            document.getElementById("MenuItem-Photography").style.display = "none";
+        }
     });
 });
